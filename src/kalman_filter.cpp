1b186dd239583f28127534536bd577244e62c9dd
rajath12 <rajath.12@gmail.com>
2020-05-21 17:42:26 -0700

    Prediction and update functions complete

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -26,16 +26,58 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+  // for lidar measurements
+
+  MatrixXd I = MatrixXd::Identity(x_.size(),x_.size());
+  MatrixXd y = z - H_ * x_;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd S_inv = S.inverse();
+  MatrixXd K = P_ * Ht * S_inv;
+
+  // measurement update
+  x_ = x_ + K * y;
+  P_ = (I - K*H_) *P_;
+
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+  // for radar measurements
+
+  // converting cartesian to polar coordinates
+  // calculate rho, phi and rho_dot
+  float rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
+  float phi = atan2(x_(1),x_(0));
+  float rho_dot; // zero case for rho needs to be dealt with
+  if (fabs(rho) < 0.0001)
+  {
+    float rho_dot = 0;
+  } else {
+    float rho_dot = (x_(0)*x_(2) + x_(1)*x_(3))/rho;
+  }
+
+  MatrixXd hx(3); // polar coordinate predictions for calculating error y
+  hx << rho, phi, rho_dot;
+  MatrixXd I = MatrixXd::Identity(x_.size(),x_.size());
+  MatrixXd y = z - hx;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd S_inv = S.inverse();
+  MatrixXd K = P_ * Ht * S_inv;
+
+  // measurement update
+  x_ = x_ + K * y;
+  P_ = (I - K*H_) *P_;
 }
